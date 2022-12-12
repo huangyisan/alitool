1960bb085750c6a17de6f9f85f57ae33b11c6d7c
Yisan Huang <anonymousyisan@gmail.com>
2022-12-12 21:01:58 +0800

    test: add unit test case for dns

--- internal/ali/dns/dns_cmd_test.go
+++ internal/ali/dns/dns_cmd_test.go
@@ -1,1 +1,45 @@
 package dns
+
+import (
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/smartystreets/goconvey/convey"
+	"reflect"
+	"testing"
+)
+
+func Test_listDnsByAccount(t *testing.T) {
+	var d *DnsClient
+	convey.Convey("Mock getAllDnsDomains", t, func() {
+		patches := gomonkey.ApplyPrivateMethod(reflect.TypeOf(d), "getAllDnsDomains", func(*DnsClient) recordDnsDomains {
+			return recordDnsDomains{"test.com": struct{}{}}
+		})
+		defer patches.Reset()
+		convey.Convey("listDnsByAccount", func() {
+			hasRecordDomains := d.listDnsByAccount()
+			convey.So(hasRecordDomains, convey.ShouldResemble, recordDnsDomains{"test.com": struct{}{}})
+		})
+
+	})
+}
+
+func Test_isDnsInAccount(t *testing.T) {
+	var d *DnsClient
+	trueCaseDomain := "test.com"
+	falseCaseDomain := "false.com"
+	convey.Convey("Mock listDnsByAccount", t, func() {
+		patches := gomonkey.ApplyPrivateMethod(reflect.TypeOf(d), "listDnsByAccount", func(*DnsClient) recordDnsDomains {
+			return recordDnsDomains{"test.com": struct{}{}}
+		})
+		defer patches.Reset()
+
+		convey.Convey("Give trueCaseDomain", func() {
+			res := d.isDnsInAccount(trueCaseDomain)
+			convey.So(res, convey.ShouldEqual, true)
+		})
+
+		convey.Convey("Give falseCaseDomain", func() {
+			res := d.isDnsInAccount(falseCaseDomain)
+			convey.So(res, convey.ShouldEqual, false)
+		})
+	})
+}
